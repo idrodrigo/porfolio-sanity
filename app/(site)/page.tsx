57cb2bb9fbf7bdb4a6f8a7844a5649c2f0e89f82
idrodrigo <idrodrigo@icloud.com>
2023-04-10 12:29:48 -0600

    fix image error

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -23,27 +23,32 @@ export default async function Home() {
 
       <h2 className="mt-16 font-bold text-gray-700 text-3xl">Projects:</h2>
       <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {projects.map((project) => (
-          <Link
-            href={`/projects/${project.slug}`}
-            key={project._id}
-            className=" border-2 border-blue-500 rounded-lg p-1 hover:scale-105 
+        {projects.map((project) => {
+          const slug = project?.slug || "";
+          const image = project?.image || "";
+          const name = project?.name || "";
+          return (
+            <Link
+              href={`/projects/${slug}`}
+              key={project._id}
+              className=" border-2 border-blue-500 rounded-lg p-1 hover:scale-105 
                       hover:border-blue-500 transition"
-          >
-            {project.image && (
-              <Image
-                src={project.image}
-                alt={project.name}
-                width={750}
-                height={300}
-                className="object-cover rounded-lg border border-green-500"
-              />
-            )}
-            <div className="mt-6 font-extrabold text-blue-500 text-4xl">
-              {project.name}
-            </div>
-          </Link>
-        ))}
+            >
+              {project.image && (
+                <Image
+                  src={image}
+                  alt={name}
+                  width={750}
+                  height={300}
+                  className="object-cover rounded-lg border border-green-500"
+                />
+              )}
+              <div className="mt-6 font-extrabold text-blue-500 text-4xl">
+                {name}
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
