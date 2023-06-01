3e0a746e9df09fa55e23bee730dbf0940afcc6f8
idrodrigo <idrodrigo@icloud.com>
2023-06-01 15:32:34 -0600

    comic

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        "Cursive": ['cursive'],
+        "Cursive": ['Comic Sans MS','cursive'],
       }
     },
   },
